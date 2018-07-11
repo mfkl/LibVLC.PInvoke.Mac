4bd01d6d595d1172c27e1928973824d0deea4e2f
Martin Finkel <martin@videolabs.io>
2018-07-11 05:27:18 +0200

    initial commit

---
+++ LibVLC.PInvoke.Mac/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using AppKit;
+
+namespace LibVLC.PInvoke.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            var libvlccoreDylibPath = Path.Combine(AppExecutionDirectory, $"lib/libvlccore.9.dylib");
+            var libvlcDylibPath = Path.Combine(AppExecutionDirectory, $"lib/libvlc.5.dylib");
+
+            var libvlccoreHandle = ObjCRuntime.Dlfcn.dlopen(libvlccoreDylibPath, 0);
+            System.Diagnostics.Debug.WriteLine(libvlccoreHandle);
+
+            var libvlcHandle = ObjCRuntime.Dlfcn.dlopen(libvlcDylibPath, 0);
+            System.Diagnostics.Debug.WriteLine(libvlcHandle);
+
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+
+        static string AppExecutionDirectory => Path.GetDirectoryName(new Uri(typeof(MainClass).Assembly.CodeBase).LocalPath);
+    }
+}
---
+++ LibVLC.PInvoke.Mac/ViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using AppKit;
+
+namespace LibVLC.PInvoke.Mac
+{
+    public partial class ViewController : NSViewController
+    {
+        const string LibraryName = "@executable_path/lib/libvlc.5.dylib";
+        //const string LibraryName = "libvlc.5";
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var libvlc = LibVLCNew();
+        }
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "libvlc_new")]
+        internal static extern IntPtr LibVLCNew(int argc = 0, IntPtr[] argv = default(IntPtr[]));
+    }
+}
