0cd7baac8420af569b05949d9bc601b1a8abf6ba
Martin Finkel <martin@videolabs.io>
2018-07-18 14:06:34 +0200

    progress...
    P/Invoke call goes fine, but plugins seem to NOT be found. Attempted to
    use xcrun -sdk macosx install_name_tool though that does not seem to
    change a thing

--- LibVLC.PInvoke.Mac/Main.cs
+++ LibVLC.PInvoke.Mac/Main.cs
@@ -8,15 +8,6 @@ namespace LibVLC.PInvoke.Mac
     {
         static void Main(string[] args)
         {
-            var libvlccoreDylibPath = Path.Combine(AppExecutionDirectory, $"lib/libvlccore.9.dylib");
-            var libvlcDylibPath = Path.Combine(AppExecutionDirectory, $"lib/libvlc.5.dylib");
-
-            var libvlccoreHandle = ObjCRuntime.Dlfcn.dlopen(libvlccoreDylibPath, 0);
-            System.Diagnostics.Debug.WriteLine(libvlccoreHandle);
-
-            var libvlcHandle = ObjCRuntime.Dlfcn.dlopen(libvlcDylibPath, 0);
-            System.Diagnostics.Debug.WriteLine(libvlcHandle);
-
             NSApplication.Init();
             NSApplication.Main(args);
         }
--- LibVLC.PInvoke.Mac/ViewController.cs
+++ LibVLC.PInvoke.Mac/ViewController.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Text;
 using AppKit;
 
 namespace LibVLC.PInvoke.Mac
 {
     public partial class ViewController : NSViewController
     {
-        //const string LibraryName = "@executable_path/lib/libvlc.5.dylib";
         const string LibraryName = "libvlc.5";
 
         public ViewController(IntPtr handle) : base(handle)
@@ -18,9 +19,23 @@ namespace LibVLC.PInvoke.Mac
             base.ViewDidLoad();
 
             var libvlc = LibVLCNew();
+
+            if (libvlc == IntPtr.Zero){
+                var error = ErrorMessage();
+                var msg = Marshal.PtrToStringAnsi(error);
+            }
         }
+       
+        static string AppExecutionDirectory => 
+        Path.GetDirectoryName(new Uri(typeof(ViewController).Assembly.CodeBase).LocalPath);
 
         [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "libvlc_new")]
         internal static extern IntPtr LibVLCNew(int argc = 0, IntPtr[] argv = default(IntPtr[]));
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "libvlc_get_version")]
+        internal static extern IntPtr LibVLCVersion();
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "libvlc_errmsg")]
+        internal static extern IntPtr ErrorMessage();
     }
 }
